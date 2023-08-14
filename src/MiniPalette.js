9cad110f822efbc800b2804c7c19362ea5501fee
Elizabeth Sotomayor <elizabethrsotomayor@gmail.com>
2023-08-14 18:05:58 -0500

    start MiniPalette and add MUI system

---
+++ src/MiniPalette.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box } from '@mui/system';
+import seedColors from './seedColors';
+
+function MiniPalette(props) {
+    
+    return (
+        <Box sx={{backgroundColor: "red"}}>
+            MiniPalette
+        </Box>
+    )
+}
+export default MiniPalette;
