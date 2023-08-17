711591706291c3566943b55937d3430d9637f823
Elizabeth Sotomayor <elizabethrsotomayor@gmail.com>
2023-08-16 19:04:16 -0500

    finish styling MiniPalettes

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,41 +1,67 @@
 import React from 'react';
 import { Box, createTheme, ThemeProvider } from '@mui/system';
-import { Link } from 'react-router-dom';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#000',
+        },
+    },
+    root: {
+        backgroundColor: "white",
+        border: "1px solid black",
+        borderRadius: "5px",
+        padding: "0.5rem",
+        position: "relative",
+        overflow: "hidden",
+        "&:hover": {
+          cursor: "pointer"
+        },
+        height: "12rem"
+    },
+      colors: {
+        backgroundColor: "#dae1e4",
+        height: "150px",
+        width: "100%",
+        borderRadius: "5px",
+        overflow: "hidden"
+    },
+      title: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        margin: "0",
+        color: "black",
+        paddingTop: "0.5rem",
+        fontSize: "1rem",
+        position: "relative"
+    },
+      emoji: {
+        marginLeft: "0.5rem",
+        fontSize: "1.5rem"
+    },
+      miniColor: {
+        height: "25%",
+        width: "20%",
+        display: "inline-block",
+        margin: "0 auto",
+        position: "relative",
+        marginBottom: "-3.5px"
+    }
+});
 
 function MiniPalette(props) {
     const { paletteName, id, emoji, colors } = {...props};
-
-    // console.log(paletteName);
-    // console.log(id);
-    // console.log(colors);
-
-    const theme = createTheme({
-        palette: {
-            primary: {
-                main: '#000',
-            },
-        },
-        custom: {
-            myOwnComponent: {
-              margin: "3rem 10px",
-              backgroundColor: "lightgreen"
-            }
-        }
-    });
     
+    const miniColorBoxes = colors.map(color => (
+        <Box key={color.name} sx={theme.miniColor} style={{backgroundColor: color.color}}/>
+      ));
+
     return (
         <ThemeProvider theme={theme}>
-            <Box sx={{ margin: (theme) => theme.custom.myOwnComponent.margin }}>
-                <Box sx={{backgroundColor: "grey"}}/>
-                <h5 sx={{ display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            margin: "0",
-            color: "black",
-            paddingTop: "0.5rem",
-            fontSize: "1rem",
-            position: "relative"}}>{paletteName} <span sx={{marginLeft: "0.5rem",
-            fontSize: "1.5rem"}}>{emoji}</span></h5>
+            <Box sx={theme.root}>
+                <Box sx={theme.colors}>{miniColorBoxes}</Box>
+                <h5 style={theme.title}>{paletteName} <span style={theme.emoji}>{emoji}</span></h5>
             </Box>
         </ThemeProvider>
     )
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,17 +1,55 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import MiniPalette from './MiniPalette';
+import { Box, createTheme, ThemeProvider } from '@mui/system';
+
+const theme = createTheme({
+  root: {
+    backgroundColor: "blue",
+    height: "100vh",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center"
+  },
+  container: {
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap"
+  },
+  nav: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
+  },
+  palettes: {
+    boxSizing: "border-box",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 30%)",
+    gridGap: "5%"
+  }
+});
 
 export default class PaletteList extends Component {
   render() {
     const {palettes} = this.props;
+
     return (
-      <div>
-        <h1>React Colors</h1>
-        {palettes.map(palette => (
-            <MiniPalette {...palette}/>
-        ))}
-      </div>
+      <ThemeProvider theme={theme}>
+        <Box style={theme.root}>
+          <Box style={theme.container}>
+            <nav style={theme.nav}>
+              <h1>React Colors</h1>
+            </nav>
+            <div style={theme.palettes}>
+              {palettes.map(palette => (
+                <MiniPalette {...palette} key={palette.id}/>
+                ))}
+            </div>
+          </Box>
+        </Box>
+      </ThemeProvider>
     )
   }
 }
